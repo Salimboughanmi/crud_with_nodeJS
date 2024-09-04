55ed27438f7a5ac6e9c2ca0fce556b49d73e8bb0
salimboughanmi <boughanmi1salim@gmail.com>
2024-09-04 23:56:42 +0100

    add all router

--- routes/product.js
+++ routes/product.js
@@ -1,2 +1,131 @@
 const express = require("express")
-const routes =  express.Router()
+const router =  express.Router()
+const Product = require('../models/product')
+
+/*  *****************************         CRUD PRODUCT       ********************** */
+
+//create POST request
+router.post('/postProduct' , (req , res)=>{
+    data = req.body
+    prod = new Product(data)
+    prod.save().then(
+        (savedprod)=>{
+        res.status(200).send(savedprod)
+    }).catch((err)=> res.status(400).send(err))
+    
+
+})
+
+
+//create POST request with async await
+router.post('/productPost' , async (req , res)=>{
+  try {
+    data = req.body
+    prod = new Product(data)
+    prodsaved = await prod.save()
+    res.status(200).send(prodsaved)
+    
+    
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+
+//create GET request
+router.get('/getProd' , (req , res)=>{
+   //myid = req.params.id
+   Product.find().then((prod)=>{
+    res.status(200).send(prod)
+   }
+).catch((err)=>{
+      res.status(400).send(err)
+   })
+})
+
+//create GET request with Async Await
+router.get('/getproduct', async (req , res)=>{
+    try {
+      
+        prod = await Product.find();
+        res.send(prod)
+        
+    } catch (error) {
+        res.send(error)
+    } })
+     
+    
+    // Create Get by ID request
+    router.get('/getproductId/:id' , (req , res)=>{
+      myId = req.params.id ;
+      Product.findById({_id : myId }).then((prod)=>{ //findOne = rechercher par name , id , username ...
+        res.send(prod)
+      }).catch((err)=>res.send(err))
+    })
+    
+    
+    // Create Get by ID request with Async Await
+    router.get('/getmyprodByID/:id' , async (req , res)=>{
+    try {
+        myid = req.params.id
+      prod = await Product.findOne({_id:myid})
+      res.status(200).send(prod)
+    
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
+    })
+    
+    
+    // Create DELETE request 
+    router.delete('/deleteprod/:id' , (req,res)=>{
+        myid =req.params.id
+        Product.findByIdAndDelete({ _id:myid}).then((prodDelete)=>{
+         
+            res.send(prodDelete)
+     
+        })
+          .catch((err)=>{
+            res.send(err)
+        })
+    })
+    
+    
+    //Create DELETE request async Await
+    router.delete('/deletemyProduct/:id' , async (req , res)=>{
+       try {
+        myid = req.params.id
+        prodDelete = await User.findByIdAndDelete({_id : myid})
+        res.send(prodDelete)
+       } catch (error) {
+        res.send(error)
+       }
+    })
+    
+    
+    // Create UPDATE request 
+    router.put('/updateprod/:id' , (req , res)=>{
+        myid = req.params.id
+        newData = req.body
+    
+        Product.findByIdAndUpdate({_id : myid} , newData).then((updated)=>{
+            res.status(200).send(updated)
+        }).catch((err)=> res.status(400).send(err))
+    } )
+    
+    
+    // Create UPDATE request async Await
+    router.put('/updateMyprod/:id' ,async (req , res)=>{
+        try {
+            myid = req.params.id
+            newdata = req.body
+            prod = await Product.findByIdAndUpdate({_id : myid} , newdata)
+            res.status(200).send(prod)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+    })
+
+
+module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,127 @@
 const express = require("express")
 const router =  express.Router()
+const User = require('../models/user')
 
+
+
+//create POST request
+router.post('/post' , (req , res)=> {
+    data = req.body
+    usr = new User(data)
+    usr.save().then(
+        (savedUser)=>{
+       res.send(savedUser)
+    }).catch(
+        (err)=> res.send(err))
+   
+}) 
+
+
+//create POST request with async await
+router.post('/postUser' , async (req , res )=> {
+    try {
+        data = req.body
+        usr = new User(data)
+       userSaved = await usr.save()
+       res.send(userSaved)
+    } catch (error) {
+        res.error
+    }
+})
+
+
+//create GET request
+router.get('/get' , (req , res )=>{
+    User.find().then((users)=>{
+        res.send(users)
+    }
+).catch(
+        (err)=>{res.send(err)} )
+})
+
+
+//create GET request with Async Await
+router.get('/getUser' , async (req , res)=>{
+try {
+    users = await User.find({ age:30 });
+    res.send(users)
+    
+} catch (error) {
+    res.send(error)
+} })
+ 
+
+// Create Get by ID request
+router.get('/getbyId/:id' , (req , res)=>{
+  myId = req.params.id ;
+  User.findById({_id : myId }).then((user)=>{ //findOne = rechercher par name , id , username ...
+    res.send(user)
+  }).catch((err)=>res.send(err))
+})
+
+
+// Create Get by ID request with Async Await
+router.get('/getUserByID/:id' , async (req , res)=>{
+try {
+    myid = req.params.id
+  user = await User.findOne({_id:myid})
+  res.send(user)
+
+} catch (error) {
+    res.send(error)
+}
+
+})
+
+
+// Create DELETE request 
+router.delete('/delete/:id' , (req,res)=>{
+    myid =req.params.id
+    User.findByIdAndDelete({ _id:myid}).then((userDelete)=>{
+     
+        res.send(userDelete)
+ 
+    })
+      .catch((err)=>{
+        res.send(err)
+    })
+})
+
+
+//Create DELETE request async Await
+router.delete('/deleteUser/:id' , async (req , res)=>{
+   try {
+    myid = req.params.id
+    myuser = await User.findByIdAndDelete({_id : myid})
+    res.send(myuser)
+   } catch (error) {
+    res.send(error)
+   }
+})
+
+
+// Create UPDATE request 
+router.put('/update/:id' , (req , res)=>{
+    myid = req.params.id
+    newData = req.body
+
+    User.findByIdAndUpdate({_id : myid} , newData).then((updated)=>{
+        res.send(updated)
+    }).catch((err)=> res.send(err))
+} )
+
+
+// Create UPDATE request async Await
+router.put('/updateUser/:id' ,async (req , res)=>{
+    try {
+        myid = req.params.id
+        newdata = req.body
+        user = await User.findByIdAndUpdate({_id : myid} , newdata)
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
 
 
 
